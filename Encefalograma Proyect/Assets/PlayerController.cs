e940d32e22f67ae43045f00458370636f4016e20
Yone <yone.moreno101@alu.ulpgc.es>
2018-01-27 11:12:40 +0000

    player is affected by gravity

---
+++ Encefalograma Proyect/Assets/PlayerController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float upSpeed = 1f;
+
+	
+	// Update is called once per frame
+	void Update () {
+        float translation = Input.GetAxis("Vertical") * upSpeed;
+        translation *= Time.deltaTime;
+        transform.Translate(0, translation, 0);
+    }
+}
