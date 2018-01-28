2279da1431848fd26b349cd2117197cb6b93a844
Yone <yone.moreno101@alu.ulpgc.es>
2018-01-28 12:01:00 +0000

    Canvas shows points

--- Encefalograma Proyect/Assets/Scripts/Player.cs
+++ Encefalograma Proyect/Assets/Scripts/Player.cs
@@ -106,7 +106,7 @@ public class Player : MonoBehaviour
        
     }
 
-    public static float getCurrentPoints()
+    public float getCurrentPoints()
     {
         return currentPoints;
     }
